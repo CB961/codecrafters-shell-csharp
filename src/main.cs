1549d047b9d90e60e06196e2596285c144525e1b
Chang Sie <43807301+CB961@users.noreply.github.com>
2025-12-04 16:16:34 +0100

    Support for printing error messages for invalid commands

--- src/main.cs
+++ src/main.cs
@@ -1,8 +1,43 @@
 class Program
 {
+    internal enum ShellCommands
+    { 
+        help,
+        echo,
+        cd
+    }
+
     static void Main()
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+
+        ReadUserInput();
+    }
+
+    private static ShellCommands? GetCommand(string command)
+    {
+        ShellCommands result;
+
+        return Enum.TryParse<ShellCommands>(command, true, out result) ? result : null;
+    }
+
+    private static void ReadUserInput()
+    {
+        string userInput;
+
+        do
+        {
+            userInput = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(userInput))
+            {
+                if (GetCommand(userInput) == null)
+                {
+                    Console.WriteLine("{0}: command not found", userInput);
+                }
+            }
+            Console.Write("$ ");
+        } while (userInput != null);
     }
 }
